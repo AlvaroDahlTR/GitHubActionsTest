deb0fd7bc992c2488abd865f6f187e0138c04f69
Alvaro Dahl <alvaro.dahl@thomsonreuters.com>
2021-02-26 03:05:40 -0300

    parallel fix

--- MAFAutomation_MAF-Application-Core/src/main/java/com/trgr/quality/maf/webdriver/WebDriverFactory.java
+++ MAFAutomation_MAF-Application-Core/src/main/java/com/trgr/quality/maf/webdriver/WebDriverFactory.java
@@ -132,15 +132,15 @@ public static boolean GridEnabled;
 			desiredCapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 			desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
 
-			options.addArguments("--headless"); // only if you are ACTUALLY running headless
-			options.addArguments("--no-sandbox");
+			//options.addArguments("--headless"); // only if you are ACTUALLY running headless
+			//options.addArguments("--no-sandbox");
 			// //https://stackoverflow.com/a/50725918/1689770
-			// options.addArguments("--disable-infobars");
+			//options.addArguments("--disable-infobars");
 			// //https://stackoverflow.com/a/43840128/1689770
-			options.addArguments("--disable-dev-shm-usage");
+			//options.addArguments("--disable-dev-shm-usage");
 			// //https://stackoverflow.com/a/50725918/1689770
 			// options.addArguments("--disable-browser-side-navigation"); // https://stackoverflow.com/a/49123152/1689770
-			options.addArguments("--disable-gpu");
+			//options.addArguments("--disable-gpu");
 			// //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
 
 			// disabled for temporaily bindu for debugging
